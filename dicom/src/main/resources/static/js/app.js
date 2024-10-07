1b4e2145c0954d3e81bd348e63f7e728f4f577b4
professorjiwon <professorjiwon@gmail.com>
2024-10-07 15:39:20 +0900

    dicom project

--- dicom/src/main/resources/static/js/app.js
+++ dicom/src/main/resources/static/js/app.js
@@ -7,14 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	const element = document.getElementById('dicomImage');
 	
 	// 이미지 요소를 초기화
-	cornerstone.enabled(element);
+	cornerstone.enable(element);
 	
 	// 이미지 얻어오기
 	const imageId = 'wadouri:img/MR.1.2.392.200036.9116.4.1.6116.40033.5.3001.1.1152393810.dcm';
 	
 	cornerstone.loadImage(imageId).then(image => {
 		cornerstone.displayImage(element, image);
-	}).cathch(err => {
+	}).catch(err => {
 		console.log('이미지 로드 실패 : ', err);
 	});
 });
